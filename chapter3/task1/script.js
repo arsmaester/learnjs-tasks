d59fb5328828ac6798d39f700e4f3200231d2133
arsmaester <daemones.ar@gmail.com>
2023-03-01 00:18:59 +0300

    chapters 1-5: done

--- chapter3/task1/script.js
+++ chapter3/task1/script.js
@@ -1,45 +1,20 @@
-// Привет, object
+// Плохой стиль
 
-let user = {};
-user.name = "John";
-user.surname = "Smith";
-user.name = "Pete";
-delete user.name;
+// подправленный код
+function pow(x, n) {
+    let result = 1;
 
-// Проверка на пустоту
-
-function isEmpty(obj) {
-    return Object.keys(obj).length === 0
-}
-
-let schedule = {};
-
-console.log(isEmpty(schedule)); // true
-
-schedule["8:30"] = "get up";
-
-console.log(isEmpty(schedule)); // false
-
-// Сумма свойств объекта
-
-
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-}
-
-let sum = 0;
-for (let key in salaries) {
-    sum += salaries[key];
-}
-
-// Умножаем все числовые свойства на 2
-
-function multiplyNumeric(obj) {
-    for (let key in obj) {
-        if (typeof obj[key] == 'number') {
-            obj[key] *= 2;
-        }
+    for (let i = 0; i < n; i++) {
+        result *= x;
     }
+
+    return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+if (n <= 0) {
+    alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`);
+} else {
+    alert(pow(x, n))
 }
